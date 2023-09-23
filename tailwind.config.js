c45312696fa3229f64e724a77bf007daafc713ff
Salman <therodfighter@gmail.com>
2023-09-24 00:23:12 +0400

    hk

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,30 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['fantasy'],
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['[data-theme=light]'],
+          'primary': '#0000',
+          'secondary': '#C58422',
+          'accent': '#37cdbe',
+          'neutral': '#3d4451',
+          'base-100': '#ffffff',
+          'base-200': '#FAF9F6',
+          'base-300': '#DBD8C5',
+          'text-base': '#FFFFFF',
+          '--rounded-box': '1rem', // border radius rounded-box utility class, used in card and other large boxes
+          '--rounded-btn': '0.5rem', // border radius rounded-btn utility class, used in buttons and similar element
+          '--rounded-badge': '1.9rem', // border radius rounded-badge utility class, used in badges and similar
+          '--animation-btn': '0.25s', // duration of animation when you click on button
+          '--animation-input': '0.2s', // duration of animation for inputs like checkbox, toggle, radio, etc
+          '--btn-text-case': 'uppercase', // set default text transform for buttons
+          '--btn-focus-scale': '0.95', // scale transform of button when you focus on it
+          '--border-btn': '1px', // border width of buttons
+          '--tab-border': '1px', // border width of tabs
+          '--tab-radius': '0.5rem', // border radius of tabs
+        },
+      },
+    ],
   },
 }
