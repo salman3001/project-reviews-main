ee9a0900c666f44fede21d50901f52203fa2a653
salman <therodfighter@gmail.com>
2023-09-26 02:10:15 +0400

    update

--- app/Controllers/Http/AdminUsersController.ts
+++ app/Controllers/Http/AdminUsersController.ts
@@ -3,6 +3,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { paginate } from 'App/Helpers/Paginate'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Hash from '@ioc:Adonis/Core/Hash'
+import Drive from '@ioc:Adonis/Core/Drive'
 
 export default class AdminUsersController {
   public async index({ view, request }: HttpContextContract) {
@@ -82,7 +83,12 @@ export default class AdminUsersController {
   }
 
   public async create({ view }: HttpContextContract) {
-    return view.render('admin/admin-users/create')
+    const roles = await prisma.role.findMany()
+    const cities = await prisma.city.findMany()
+    const states = await prisma.state.findMany()
+    const country = await prisma.country.findMany()
+
+    return view.render('admin/admin-users/create', { roles, cities, states, country })
   }
 
   public async store({ request, response, session }: HttpContextContract) {
@@ -93,7 +99,8 @@ export default class AdminUsersController {
       lastName: schema.string({ trim: true }),
       phone: schema.string.optional({ trim: true }, [rules.minLength(8)]),
       password: schema.string({ trim: true }, [rules.minLength(8), rules.alphaNum()]),
-
+      isActive: schema.string.optional(),
+      roleId: schema.string.optional(),
       address: schema.object().members({
         address: schema.string.optional(),
         cityId: schema.number.optional(),
@@ -131,6 +138,8 @@ export default class AdminUsersController {
         firstName: payload.firstName,
         lastName: payload.lastName,
         phone: payload.phone,
+        isActive: payload.isActive ? JSON.parse(payload.isActive) : false,
+        roleId: Number(payload.roleId),
         address: {
           create: { ...payload.address },
         },
@@ -151,15 +160,96 @@ export default class AdminUsersController {
       })
     }
 
-    session.flash('message', 'User Created Successfully')
+    session.flash('message', { type: 'success', title: 'User Created Successfully' })
     return response.redirect().toRoute('admin_users.index')
   }
 
   public async show({}: HttpContextContract) {}
 
-  public async edit({}: HttpContextContract) {}
+  public async edit({ view, params }: HttpContextContract) {
+    const user = await prisma.adminUser.findUnique({
+      where: { id: Number(params.id) },
+      include: { address: true, Social: true, avatar: true },
+    })
 
-  public async update({}: HttpContextContract) {}
+    const cities = await prisma.city.findMany()
+    const states = await prisma.state.findMany()
+    const countries = await prisma.country.findMany()
+    const roles = await prisma.role.findMany()
+
+    return view.render('admin/admin-users/edit', { user, cities, states, countries, roles })
+  }
+
+  public async update({ request, response, session, params }: HttpContextContract) {
+    const id = Number(params.id)
+    const validationSchema = schema.create({
+      image: schema.file.optional({ extnames: ['jpg', 'jpeg', 'png', 'webp', 'gif'], size: '2mb' }),
+      firstName: schema.string({ trim: true }),
+      lastName: schema.string({ trim: true }),
+      phone: schema.string.optional({ trim: true }, [rules.minLength(8)]),
+      isActive: schema.string.optional(),
+      roleId: schema.string.optional(),
+      address: schema.object().members({
+        address: schema.string.optional(),
+        cityId: schema.number.optional(),
+        stateId: schema.number.optional(),
+        countryId: schema.number.optional(),
+        zip: schema.string.optional(),
+      }),
+      social: schema.object().members({
+        website: schema.string.optional(),
+        facebook: schema.string.optional(),
+        twitter: schema.string.optional(),
+        instagram: schema.string.optional(),
+        pintrest: schema.string.optional(),
+        vk: schema.string.optional(),
+        whatsapp: schema.string.optional(),
+        telegram: schema.string.optional(),
+      }),
+    })
+
+    const payload = await request.validate({ schema: validationSchema })
+
+    const user = await prisma.adminUser.update({
+      where: { id },
+      data: {
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        phone: payload.phone,
+        isActive: payload.isActive ? JSON.parse(payload.isActive) : false,
+        roleId: Number(payload.roleId),
+        address: {
+          create: { ...payload.address },
+        },
+        Social: { create: { ...payload.social } },
+      },
+      include: { avatar: true },
+    })
+
+    if (payload.image) {
+      await payload.image.moveToDisk('./admin_users/', {
+        name: user.firstName + Date.now() + payload.image.extname,
+      })
+      const imageName = payload.image?.fileName
+      const createdImage = await prisma.image.create({
+        data: { url: '/uploads/admin_users/' + imageName, url_sm: '' },
+      })
+
+      await prisma.adminUser.update({
+        where: { id: user.id },
+        data: { avatar: { update: { id: createdImage.id } } },
+      })
+
+      if (user?.avatar) {
+        const deletedImage = await prisma.image.delete({ where: { id: user.avatar.id } })
+        Drive.delete(deletedImage.url)
+      }
+    }
+
+    session.flash('message', { type: 'success', title: 'User updated successfully' })
+
+    return response.redirect().toRoute('admin_users.index')
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
